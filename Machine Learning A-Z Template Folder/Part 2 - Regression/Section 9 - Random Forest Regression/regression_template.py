bd2a841a12d1c84b76061e5783179c334f130cc2
Brock <brockpaullandry@gmail.com>
2018-05-20 00:16:24 -0500

    added the hierarchy for the Mach Vision Tut

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 9 - Random Forest Regression/regression_template.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 9 - Random Forest Regression/regression_template.py
@@ -1,4 +1,4 @@
-# Decision Tree Regression
+# Regression Template
 
 # Importing the libraries
 import numpy as np
@@ -22,10 +22,8 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting the decision tree regression to the dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
-regressor.fit(X, y)
+# Fitting the Regression Model to the dataset
+# Create your regressor here
 
 # Predicting a new result
 y_pred = regressor.predict(6.5)
@@ -39,7 +37,7 @@ plt.ylabel('Salary')
 plt.show()
 
 # Visualising the Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
